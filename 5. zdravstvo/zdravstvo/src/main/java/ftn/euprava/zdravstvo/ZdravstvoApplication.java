5c0344cdc3974823c454200c2c6141672c239946
Jelena Mihajlovic <starglitz47@gmail.com>
2022-03-12 09:05:23 +0100

    initialize spring boot & react app

---
+++ 5. zdravstvo/zdravstvo/src/main/java/ftn/euprava/zdravstvo/ZdravstvoApplication.java
@@ -0,0 +1,13 @@
+package ftn.euprava.zdravstvo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZdravstvoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZdravstvoApplication.class, args);
+	}
+
+}
---
+++ 5. zdravstvo/zdravstvo/src/test/java/ftn/euprava/zdravstvo/ZdravstvoApplicationTests.java
@@ -0,0 +1,13 @@
+package ftn.euprava.zdravstvo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZdravstvoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
