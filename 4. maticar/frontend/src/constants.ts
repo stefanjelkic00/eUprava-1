a64618706391509bc112294b17263f843baea4ce
Dušan Uverić <dusan.uveric@makit.lv>
2022-03-20 17:13:20 +0100

    maticar/frontend: add env for auth server url
    Signed-off-by: Dušan Uverić <dusan.uveric@makit.lv>

--- 4. maticar/frontend/src/constants.ts
+++ 4. maticar/frontend/src/constants.ts
@@ -1,3 +1,4 @@
-export const AUTH_SERVER_URL = 'http://localhost:3101';
+// export const AUTH_SERVER_URL = 'http://localhost:3101';
+export const AUTH_SERVER_URL = import.meta.env.VITE_AUTH_SERVER_URL;
 export const MATICAR_BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 export const PUBLIC_URL = import.meta.env.VITE_APP_URL;
--- 4. maticar/frontend/src/vite-env.d.ts
+++ 4. maticar/frontend/src/vite-env.d.ts
@@ -3,6 +3,7 @@
 interface ImportMetaEnv {
   readonly VITE_APP_URL: string;
   readonly VITE_BACKEND_URL: string;
+  readonly VITE_AUTH_SERVER_URL: string;
 }
 
 interface ImportMeta {
